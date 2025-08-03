1aa3d538c2fbb6bd72220809d31371a7258886c5
TuralPiriyev <144872220+TuralPiriyev@users.noreply.github.com>
2025-08-03 12:46:07 +0400

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -49,7 +49,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:3000'
     ],
-    credentials: true,
+    
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     preflightContinue: false,
